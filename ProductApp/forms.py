8d9abf66909c434590230bc49a331f84eb0df523
Md Sha Newaz <sawonnstu@gmail.com>
2024-01-11 00:26:57 +0600

    11Jan

---
+++ ProductApp/forms.py
@@ -0,0 +1,10 @@
+from django import forms
+from ProductApp.models import ProductModle, CategoryModel, BrandModel, WarrantyModel
+
+# Forms Stat here
+
+class ProductSearchForm(forms.Form):
+    category = forms.ModelChoiceField(queryset=CategoryModel.objects.all(), empty_label="All Categories", required=False)
+    brand = forms.ModelChoiceField(queryset=BrandModel.objects.all(), empty_label="All Brands", required=False)
+    warranty = forms.ModelChoiceField(queryset=WarrantyModel.objects.all(), empty_label="All Warranties", required=False)
+    # price = forms.IntegerField(label='Price Range')
--- ProductApp/views.py
+++ ProductApp/views.py
@@ -1,12 +1,43 @@
 from django.shortcuts import render, HttpResponse
 from ProductApp.models import ProductModle
+from ProductApp.forms import ProductSearchForm
 
 # Create your views here.
 
 def home_view(request):
     products = ProductModle.objects.all()
+    form = ProductSearchForm(request.GET)
+
+    if form.is_valid():
+        category = form.cleaned_data.get('category')
+        brand = form.cleaned_data.get('brand')
+        warranty = form.cleaned_data.get('warranty')
+
+        #products = ProductModle.objects.filter(category=category, brand=brand, warranty=warranty)
+
+        if category:
+            products = ProductModle.objects.filter(category=category)
+        if brand:
+            products = ProductModle.objects.filter(brand=brand)
+        if warranty:
+            products = ProductModle.objects.filter(warranty=warranty)
+        if category and brand:
+            products = ProductModle.objects.filter(category=category, brand=brand)
+        if category and warranty:
+            products = ProductModle.objects.filter(category=category, warranty=warranty)
+        if brand and warranty:
+            products = ProductModle.objects.filter(brand=brand, warranty=warranty)
+        if category and brand and warranty:
+            products = ProductModle.objects.filter(category=category, brand=brand, warranty=warranty)
+            
+
+    else:
+        products = ProductModle.objects.all()
+
     context = {
-        'products' : products,
+        'products': products,
+        'form': form,
         'title': 'HOME',
     }
+
     return render(request, 'ProductApp/home.html', context=context)
